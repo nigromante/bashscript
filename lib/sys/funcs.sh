d37f6cddc998fac7f8cff03cb0d1656f607f98b2
Nigromante <nigromante@github.com>
2026-03-23 23:34:55 -0300

    fix funciones

--- lib/sys/funcs.sh
+++ lib/sys/funcs.sh
@@ -6,7 +6,7 @@ unescape() {
     local patron2='s/\\[0-9]*\[[0-9]*;[0-9]*\m//g'
     local linea="$(echo $1 | sed -e $patron1)"
     linea="$(echo $linea | sed -e $patron2)"
-    echo $linea
+    echo "$linea"
 }
 
 mklinea() {
@@ -22,13 +22,13 @@ mklinea() {
     linea="$( echo $linea | sed -e $patron3 )"
     linea="$( echo $linea | sed -e $patron4 )"
     linea="$( echo $linea | sed -e $patron5 )"
-    echo $linea
+    echo "$linea"
   }
   
   replaceChar() {
     local patron0="s/$2/$3/g"
     local linea="$( echo $1 | sed -e $patron0 )"
-    echo $linea
+    echo "$linea"
 }
 
 getLongest() {
@@ -73,24 +73,21 @@ normalizeList() {
 }
 
 
+#  str=$( padright "julian" 20 ) 
 padright() {
-  texto="$1"
-  len=$2
-  echo "$( printf '%*s' $len "$texto" )"
+  echo "$( printf '%*s' $2 "$1" )"
 }
 
+#  str=$( padleft "julian" 20 ) 
 padleft() {
-  texto="$1"
-  len=$2
-  echo "$( printf '%-*s' $len "$texto" )"
+  echo "$( printf '%-*s' $2 "$1" )"
 }
 
+#  str=$( padcenter "vidal" 20 ) 
 padcenter() {
-  texto="$1"
-  len=$2
-  resto=$(( ($len + ${#texto}) / 2 ))
-  str1=$( padright "$texto" $resto ) 
-  str2=$( padleft  "$str1"  $len ) 
+  resto=$(( ($2 + ${#1}) / 2 ))
+  str1=$( padright "$1" $resto ) 
+  str2=$( padleft  "$str1"  $2 ) 
   echo "$str2" 
 }
 
