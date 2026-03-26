14ef2ca493aa735da5415dc3ca648c46bba1c76d
Nigromante <nigromante@github.com>
2026-03-26 19:54:19 -0300

    rename choices plugins

--- lib/choices/choices.sh
+++ lib/choices/choices.sh
@@ -199,7 +199,7 @@ choices_themeshape_list() {
 # ------------------------------------- RUN
 choices_run() {
 
-    moduleLoadLazy "lazy_${plugin}"
+    moduleLoadLazy "choices_plugins/${plugin}"
     choices_menu_loader
     choices_menu_run
 }
