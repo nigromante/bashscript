205f74d9cff343e330dac9cf276294c04e58f8ab
Nigromante <nigromante@github.com>
2026-03-27 03:43:50 -0300

    net rename functions

--- lib/sys/net.sh
+++ lib/sys/net.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-getIp() {
+net_getIp() {
     IP=$(ifconfig | grep inet | grep 255.255.255.0 | awk '{print $2"\n"}')
     echo $IP
 }
 
-getIpMask() {
+net_getIpMask() {
     IP=$(ifconfig | grep inet | grep 255.255.255.0 | awk '{print $2}' | awk -F'.' '{printf $1"."$2"."$3".*\n"}' )
     echo $IP
 }
 
 
-getHostName() {
+net_getHostName() {
     HOSTNAME=$(uname -n)
     echo $HOSTNAME
 }
 
 
-nmap_neighborhood() {
+net_nmap() {
     NMAP=$(nmap -v -sn $1 | grep -v "host down" | grep "report for" | awk '{print $5 $6}') 
     for OUTPUT in $NMAP
     do
