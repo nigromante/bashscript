af6bcc67c95f556116cb320fa6b48a38608de43d
Nigromante <nigromante@github.com>
2026-04-20 15:52:29 -0400

    improvements to installer

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+figlet "ezbash - install"
 
 cat ~/work/bashscript/launch/aliases.sh >> ~/.dotfiles/aliases.sh
 
-mkdir -p ~/.local/share/bin/apps
-mkdir -p ~/.local/share/lib/ezbash
+[[ -d "~/.local/share/bin/apps" ]]   && mkdir -p ~/.local/share/bin/apps
+[[ -d "~/.local/share/lib/ezbash" ]] && mkdir -p ~/.local/share/lib/ezbash
 
 cp -R ~/work/bashscript/launch/ezbashrun.sh ~/.local/share/bin
 cp -R ~/work/bashscript/apps/*   ~/.local/share/bin/apps
